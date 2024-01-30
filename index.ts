44d28300e1a5a2acc31e1700aefd93b8180d7f87
Carlos Vanzego <129208650+CarlosVanzego@users.noreply.github.com>
2024-01-30 01:03:53 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express'; // Importing Express framework and types from Express library
+import dotenv from 'dotenv'; // Importing dotenv library for environment variables
 
-dotenv.config();
+dotenv.config(); // Loading environment variables from .env file
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express(); // Creating an instance of Express application
+const port = process.env.PORT; // Getting the port from environment variables
 
+// Setting up a route for the root URL
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server'); // Sending a response with a message to the client
 });
 
+// Starting the server and listening on the specified port
 app.listen(port, () => {
-    console.log(` [server]: Server is running at http://localhost:${port}`);
+    console.log(` [server]: Server is running at http://localhost:${port}`); // Logging a message to indicate that the server is running
 });
-
